4b311afa3b7dabb4b0b0ef94824a6cd41684800f
OtaviOuu <conta.otaviou@gmail.com>
2024-11-04 01:01:43 -0300

    setup

---
+++ src/lib/components/UserInput.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+
+    interface UserInputInterface {
+        children: Snippet
+        userName: string
+    }
+
+    let {userName, children}: UserInputInterface = $props()
+    let name = $state(userName)
+</script>
+
+
+<h1>Digite</h1>
+<input type="text" bind:value={name}>
+<p>{name}</p>
+
+{@render children()}
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+    interface slot {
+        children: Snippet
+    }
+
+    import "../app.css";
+
+
+
+    let {children}:slot = $props()
+</script>
+
+{@render children()}
